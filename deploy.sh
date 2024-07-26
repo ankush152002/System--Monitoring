8e760dcb8343641398a4a77561660c99960b2246
Ankush Singh <98583838+ankush152002@users.noreply.github.com>
2024-07-26 18:34:22 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,9 @@
 # Pull the latest image
 docker pull ankush152002/monitoring-app:latest
 
-# Stop any running container using port 5000
-docker ps --filter "ancestor=ankush152002/monitoring-app" --filter "status=running" --format "{{.ID}}" | xargs -I {} docker stop {}
-
-# Remove any stopped containers
-docker ps -a --filter "ancestor=ankush152002/monitoring-app" --filter "status=exited" --format "{{.ID}}" | xargs -I {} docker rm {}
+# Stop and remove any running container using port 5000
+docker ps --filter "publish=5000" --format "{{.ID}}" | xargs -r docker stop | xargs -r docker rm
 
 # Run the new container
 docker run -d -p 5000:5000 ankush152002/monitoring-app:latest
+

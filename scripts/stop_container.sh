67660fb49793d2de6c806267b118a2b570059d57
Ankush Singh <98583838+ankush152002@users.noreply.github.com>
2025-02-07 11:08:04 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,3 +3,14 @@ set -e
 
 # Stop the running container (if any)
 echo "Hi"
+
+# Unused variable (SonarQube should flag this)
+UNUSED_VAR="This variable is never used"
+
+# Bad practice: Useless use of 'echo'
+echo $(ls)   # Instead of 'ls' directly, useless use of 'echo'
+
+# Security issue: Command substitution without quotes (may cause word splitting)
+FILENAME=$(ls /tmp)  # This can break if filenames have spaces
+
+echo "File in /tmp: $FILENAME"   # Issue: Unquoted variable expansion
